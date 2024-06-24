228d26baa03f7d76cf326a78f2bca72a75a29cae
vrathish <veenarathish@gmail.com>
2024-06-24 18:41:27 +0530

    Initial Commit

---
+++ src/main/java/com/example/rathish/RathishApplication.java
@@ -0,0 +1,13 @@
+package com.example.rathish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RathishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RathishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rathish/RathishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rathish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RathishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
